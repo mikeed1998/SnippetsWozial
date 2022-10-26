9abafdf0720f6f96bc7f69b23ea61b30df5b8010
Michael <mikeed1998@gmail.com>
2022-10-26 16:56:19 -0500

    Agregando snippet para barra de busqueda con AJAX y PHP

---
+++ SNIPPET_server_side_filter_jquery_ajax_php_mysql/getData.php
@@ -0,0 +1,63 @@
+<?php 
+// Include configuration file 
+require_once 'config.php'; 
+ 
+// Include User class 
+require_once 'User.class.php'; 
+ 
+// Initialize User class 
+$user = new User(); 
+ 
+// Define filters 
+$conditions = array(); 
+ 
+// If search request is submitted 
+if(!empty($_POST['keywords'])){ 
+    $conditions['search'] = array('name' => $_POST['keywords'], 'email' => $_POST['keywords']); 
+} 
+ 
+// If filter request is submitted 
+if(!empty($_POST['filter'])){ 
+    $sortVal = $_POST['filter']; 
+    $sortArr = array( 
+        'new' => array( 
+            'order_by' => 'created DESC' 
+        ), 
+        'asc'=>array( 
+            'order_by'=>'name ASC' 
+        ), 
+        'desc'=>array( 
+            'order_by'=>'name DESC' 
+        ), 
+        'active'=>array( 
+            'where'=>array('status' => 1) 
+        ), 
+        'inactive'=>array( 
+            'where'=>array('status' => 0) 
+        ) 
+    ); 
+    $sortKey = key($sortArr[$sortVal]); 
+    $conditions[$sortKey] = $sortArr[$sortVal][$sortKey]; 
+} 
+ 
+// Get members data based on search and filter 
+$members = $user->getRows($conditions); 
+ 
+if(!empty($members)){ 
+    $i = 0; 
+    foreach($members as $row){ $i++; 
+        echo '<tr>'; 
+        echo '<td>'.$i.'</td>'; 
+        echo '<td>'.$row['name'].'</td>'; 
+        echo '<td>'.$row['email'].'</td>'; 
+        echo '<td>'.$row['gender'].'</td>'; 
+        echo '<td>'.$row['country'].'</td>'; 
+        echo '<td>'.$row['created'].'</td>'; 
+        $status = ($row['status'] == 1)?'Active':'Inactive'; 
+        echo '<td>'.$status.'</td>'; 
+        echo '</tr>'; 
+    } 
+}else{ 
+    echo '<tr><td colspan="7">No members(s) found...</td></tr>'; 
+} 
+exit;
---
+++ SNIPPET_server_side_filter_jquery_ajax_php_mysql/index.php
@@ -0,0 +1,99 @@
+<?php 
+// Include configuration file 
+require_once 'config.php'; 
+ 
+// Include User class 
+require_once 'User.class.php'; 
+ 
+// Initialize User class 
+$user = new User(); 
+ 
+// Get members data from database 
+$members = $user->getRows(); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>ServerSide</title>
+    <script>
+        function searchFilter(){
+            $.ajax({
+                type: 'POST',
+                url: 'getData.php',
+                data: 'keywords='+$('#searchInput').val()+'&filter='+$('#filterSelect').val(),
+                
+                beforeSend: function(){
+                    $('.loading-overlay').show();
+                },
+                success:function(html){
+                    $('.loading-overlay').hide();
+                    $('#userData').html(html);
+                }
+            });
+        }
+    </script>
+</head>
+<body>
+<div class="search-panel">
+    <div class="input-group">
+        <input type="text" class="search form-control" id="searchInput" placeholder="By Name or Email">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" onclick="searchFilter();">Search</button>
+        </div>
+    </div>
+    <div class="form-group">
+        <select class="form-control" id="filterSelect" onchange="searchFilter();">
+            <option value="">Sort By</option>
+            <option value="new">Newest</option>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+            <option value="active">Active</option>
+            <option value="inactive">Inactive</option>
+        </select>
+    </div>
+</div>
+
+<div class="main-content">
+    <div class="loading-overlay" style="display: none;"><div class="overlay-content">Loading.....</div></div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Country</th>
+                <th>Created</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody id="userData">
+            <?php 
+            if(!empty($members)){ $i = 0; 
+                foreach($members as $row){ $i++; 
+            ?>
+            <tr>
+                <td><?php echo $i; ?></td>
+                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['gender']; ?></td>
+                <td><?php echo $row['country']; ?></td>
+                <td><?php echo $row['created']; ?></td>
+                <td><?php echo ($row['status'] == 1)?'Active':'Inactive'; ?></td>
+            </tr>
+            <?php } }else{ ?>
+            <tr><td colspan="7">No member(s) found...</td></tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
+
+
