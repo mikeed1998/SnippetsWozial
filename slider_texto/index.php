1ebe4c3c4548493facb6f9cc8918c315f57ddafe
Michael Eduardo Sandoval Perez <mikeed1998@gmail.com>
2023-01-26 00:57:02 -0600

    selecciones-takasami-logica

---
+++ slider_texto/index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>titulo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css" rel="stylesheet"></link>
+    <style>
+    </style>
+</head>
+<body style="background: none !important;">
+    <div class="container-fluid">
+        <div class="row position-relative">
+            <div class="col-12 text-end position-absolute top-0 start-50 translate-middle-x z-3">
+                <div class="col-6 mt-4 text-start position-relative bg-warning" style="max-height: 633px; overflow: scroll;" id="categorias_base">
+                Hoteles
+                    <ul>
+                        <li>1 Lorem, ipsum dolor.</li>
+                        <li>2 Lorem, ipsum dolor.</li>
+                        <li>3 Lorem, ipsum dolor.</li>
+                        <li>4 Lorem, ipsum dolor.</li>
+                    </ul>
+
+                    Restaurantes
+                    <ul>
+                        <li>1 Lorem, ipsum dolor.</li>
+                        <li>2 Lorem, ipsum dolor.</li>
+                        <li>3 Lorem, ipsum dolor.</li>
+                        <li>4 Lorem, ipsum dolor.</li>
+                    </ul>
+
+                    Restaurantes
+                    <ul>
+                        <li>1 Lorem, ipsum dolor.</li>
+                        <li>2 Lorem, ipsum dolor.</li>
+                        <li>3 Lorem, ipsum dolor.</li>
+                        <li>4 Lorem, ipsum dolor.</li>
+                    </ul>
+                    Restaurantes
+                    <ul>
+                        <li>1 Lorem, ipsum dolor.</li>
+                        <li>2 Lorem, ipsum dolor.</li>
+                        <li>3 Lorem, ipsum dolor.</li>
+                        <li>4 Lorem, ipsum dolor.</li>
+                    </ul>
+                    Restaurantes
+                    <ul>
+                        <li>1 Lorem, ipsum dolor.</li>
+                        <li>2 Lorem, ipsum dolor.</li>
+                        <li>3 Lorem, ipsum dolor.</li>
+                        <li>4 Lorem, ipsum dolor.</li>
+                    </ul>
+                    
+                </div>
+                <div class="col-6"></div>
+            </div>
+            <div class="col-12 text-end position-absolute top-100 start-50 translate-middle-x z-2">
+                <div id="auxiliar">
+                    <div>   
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"></div>
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                <img src="ocean.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <button id="mover"> * * * </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 text-start">
+                                <h1>Titulo</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta minus quasi, illum delectus aliquam excepturi laudantium ad ipsa molestiae quaerat culpa facere id velit maiores omnis perferendis at, exercitationem nihil!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>   
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"></div>
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                <img src="ocean.png" alt="" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <button id="mover"> * * * </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 text-start">
+                                <h1>Titulo</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta minus quasi, illum delectus aliquam excepturi laudantium ad ipsa molestiae quaerat culpa facere id velit maiores omnis perferendis at, exercitationem nihil!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 bg-warning">
+                Secciones    
+                <ul>
+                    <li>subseccion</li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <div id="auxiliar">
+                    <div>
+                    <img src="ocean.png" alt="" class="img-fluid">
+                    </div>
+                    <div>
+                    <img src="ocean.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div id="auxiliar">
+                    <div>
+                    <h1>Titulo</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta minus quasi, illum delectus aliquam excepturi laudantium ad ipsa molestiae quaerat culpa facere id velit maiores omnis perferendis at, exercitationem nihil!</p>
+                    </div>
+                    <div>
+                    <h1>Titulo</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta minus quasi, illum delectus aliquam excepturi laudantium ad ipsa molestiae quaerat culpa facere id velit maiores omnis perferendis at, exercitationem nihil!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-9 mx-auto">
+                <div id="slider">
+                    <div>
+                        Hola
+                    </div>
+                    <div>
+                        Mundo
+                    </div>
+                    <div>
+                        Adios
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div id="dos">
+                    <div>asa</div>
+                    <div>fsfs</div>
+                    <div>dffd</div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <script src="jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script>     
+        $('#slider, #dos, #auxiliar').slick({
+            // dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            nextArrow: '#mover',
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        // dots: true
+                    } 
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+    </script>
+</body>
+</html>
